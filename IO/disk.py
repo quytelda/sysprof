28a22178c4f7b0083c34faaf256c4705621791f2
Roger Xiao <rxiao3@illinois.edu>
2016-05-07 16:55:09 -0500

    Create disk.py

---
+++ IO/disk.py
@@ -0,0 +1,14 @@
+with open('disk.txt') as f:
+    f.readline()
+    f.readline()
+    f.readline()
+    line = f.readline()
+    while line:
+
+        if(line != '\n'):
+
+                nums = [float(x) for x in line.split()[1:6]]
+
+                print "%f  %f  %f  %f  %f" %(nums[0], nums[1], nums[2], nums[3], nums[4])
+
+        line = f.readline()
