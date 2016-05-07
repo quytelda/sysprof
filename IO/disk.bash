a4a5b9675dfb8bc30e0a0d8e25e8fd3c9777022e
Roger Xiao <rxiao3@illinois.edu>
2016-05-07 18:17:50 -0500

    Update disk.bash

--- IO/disk.bash
+++ IO/disk.bash
@@ -5,7 +5,7 @@ for i in {1..100}
 do
         #sar -B 1 1 > disk.txt
         iostat -d > disk.txt
-        python disk.py
-        echo ""
+        python disk.py >> disk_output.txt
+        echo "" >> disk_output.txt
         sleep 15
 done
