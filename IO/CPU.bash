bcb3f200a4c5543f4e4a1298049f4c6abcaedbb2
Roger Xiao <rxiao3@illinois.edu>
2016-05-07 18:17:24 -0500

    Update CPU.bash

--- IO/CPU.bash
+++ IO/CPU.bash
@@ -3,7 +3,7 @@
 for i in {1..100}
 do
         sar 1 1 > CPU.txt
-        python CPU.py
-        echo ""
+        python CPU.py >> CPU_output.txt
+        echo "" >> CPU_output.txt
         sleep 15
 done
