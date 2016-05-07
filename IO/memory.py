9870c9988e0777c290615aadf651dc4285ac47b4
Roger Xiao <rxiao3@illinois.edu>
2016-05-07 17:10:48 -0500

    Update memory.py

--- IO/memory.py
+++ IO/memory.py
@@ -2,5 +2,7 @@ with open('memory.txt') as f:
     f.readline()
     nums = [int(x) for x in f.readline().split()[1:7]]
 
-for i in range(0, 6):
-        print nums[i]
+print "%f  %f  %f  %f  %f  %f" %(nums[0], nums[1], nums[2], nums[3], nums[4], nums[5])
+
+#for i in range(0, 6):
+#        print nums[i]
