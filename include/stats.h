1f53f38577572f42e02f99c5fb75846e76981adc
Quytelda Kahja <quytelda@tamalin.org>
2016-05-07 21:51:23 -0400

    Refactor shared data structures.

--- include/stats.h
+++ include/stats.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * stats.h - Linux Statistical System Profilerure
  * Definitions for shared data structures.
  *
@@ -24,20 +24,18 @@ struct timespec {
     long    tv_nsec;    /* nanoseconds */
 };
 
-struct FS {
-    struct timespec timestamp;
-    int UID;
-    unsigned int R;
-    unsigned int W;
-    unsigned int X;
-};
-
-struct NET {
-    struct timespec timestamp;
+struct nf_data {
+    //inputs
     unsigned int pac_in;
-    unsigned int pac_out;
-};
+    unsigned int udp_in;
+    unsigned int tcp_in;
+    unsigned int icmp_in;
+    unsigned int other_in;
 
-#typedef TIMESPEC_DEFAULT struct timespec {0, 0};
-#typedef FS_DEFAULT  struct FS  {TIMESPEC_DEFAULT,0,0,0,0};
-#typedef NET_DEFAULT struct NET {TIMESPEC_DEFUALT,0,0};
+    //outputs
+    unsigned int pac_out;
+    unsigned int udp_out;
+    unsigned int tcp_out;
+    unsigned int icmp_out;
+    unsigned int other_out;
+};
