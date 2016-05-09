7e2e8c8972af46a0e369ea918ff0462dc1a77677
Erumpet <erumpet@gmail.com>
2016-05-08 22:29:39 -0500

    minor fixes to gather

--- daemon/gather.c
+++ daemon/gather.c
@@ -3,54 +3,71 @@
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sqlite3.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include "../include/stats.h"
 
 int main(){
 
-    //Constants
-    int PAGE_SIZE = getpagesize();
-    size_t FILESIZE = PAGE_SIZE * 32;
-    char * FILEPATH = "/dev/sysprof";
-    sigset_t mask;
-    int index = 0;
+	//	Constants
+	int PAGE_SIZE = getpagesize();
+	size_t FILESIZE = PAGE_SIZE * 32;
+	char * FILEPATH = "/dev/sysprof";
+	sigset_t mask;
+	int index = 0;
 
-    //Setup to recieve signal from kernel module
-    //sigfillset(&mask);
-    //sigdelset(&mask, SIGRTMIN+1);
-    //sigsuspend(&mask);
+	//	Setup to recieve signal from kernel module
+	sigfillset(&mask);
+	sigdelset(&mask, SIGCONT);
+	sigsuspend(&mask);
 
-    // Get a file descriptor to the char device.
-    int fd = open(FILEPATH, O_RDONLY|O_SYNC);
-    if (fd < 0)
-    {
-	perror("Error opening file for reading.");
-	exit(EXIT_FAILURE);
-    }
+	//	Set up database connection
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+	rc = sqlite3_open("gather.db", &db);
+	if( rc ){
+  	fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+  	exit(0);
+  }
+  
 
-    // memory map the contents
-    void * map = mmap(0, FILESIZE, PROT_READ, MAP_SHARED, fd, 0);
-    if(!map)
-    {
-	fprintf(stderr, "Failed to mmap device file.\n");
-	return -1;
-    }
+	//	Get a file descriptor to the char device.
+	int fd = open(FILEPATH, O_RDONLY|O_SYNC);
+	if (fd < 0)
+	{
+		perror("Error opening file for reading.");
+		exit(EXIT_FAILURE);
+	}
 
-    struct nf_data * all = (struct nf_data *) map;
-    //for(;;) {
-	//Wait on signal
-	//(void) sigsuspend(&mask);
+	//	Memory map the shared memory area
+	void * map = mmap(0, FILESIZE, PROT_READ, MAP_SHARED, fd, 0);
+	if(!map)
+	{
+		fprintf(stderr, "Failed to mmap device file.\n");
+		return -1;
+	}
+	struct nf_data * all = (struct nf_data *)map;
 
-	// extract data
-	if(map + (index + 1) * sizeof(struct nf_data) > map + FILESIZE)
-	    index = 0;
-	struct nf_data current = all[index];
-	index++;
+	//	Loop and wait for signal
+	for(;;) 
+	{
+		(void) sigsuspend(&mask);
 
-	//Use data
-	printf("pack in %u\npack out %u\n", current.pac_in, current.pac_out);
-//    }
+		//	Extract data
+		if(map + (index + 1) * sizeof(struct nf_data) > map + FILESIZE)
+			index = 0;
+		struct nf_data current = all[index];
+		index++;
 
-    return 0;
+		//	Use data
+		printf("pack in %u\npack out %u\n", current.pac_in, current.pac_out);
+	}
+
+	//	Clean up and exit
+	sqlite3_close(db);
+	munmap(map, FILESIZE);
+	close(fd);
+	return 0;
 }
