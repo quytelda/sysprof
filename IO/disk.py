b5f11fa5c81899254e5264e803cfb93905aef8d9
Roger Xiao <rxiao3@illinois.edu>
2016-05-08 21:16:08 -0500

    Update disk.py

--- IO/disk.py
+++ IO/disk.py
@@ -9,6 +9,6 @@ with open('disk.txt') as f:
 
                 nums = [float(x) for x in line.split()[1:6]]
 
-                print "%f  %f  %f  %f  %f" %(nums[0], nums[1], nums[2], nums[3], nums[4])
+                print ("%f  %f  %f  %f  %f" %(nums[0], nums[1], nums[2], nums[3], nums[4]))
 
         line = f.readline()
