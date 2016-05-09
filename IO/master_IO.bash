496d3de2863f66c3a7b27f325b4d26dd56713afb
Roger Xiao <rxiao3@illinois.edu>
2016-05-08 20:30:53 -0500

    Update master_IO.bash

--- IO/master_IO.bash
+++ IO/master_IO.bash
@@ -1,19 +1,18 @@
-
 #!/bin/bash
 
 for i in {1..100}
 do
         sar 1 1 > CPU.txt
-        python CPU.py >> CPU_output.txt
-        echo "" >> CPU_output.txt
+        python CPU.py >> CPU_pipe
+        echo "Wrote to cpu"
 
         iostat -d > disk.txt
-        python disk.py >> disk_output.txt
-        echo "" >> disk_output.txt
+        python disk.py >> disk_pipe
+        echo "Wrote to disk"
 
         free -m > memory.txt
-        python memory.py >> memory_output.txt
-        echo "" >> memory_output.txt
+        python memory.py >> memory_pipe
+        echo "Wrote to memory"
 
         sleep 60
 done
