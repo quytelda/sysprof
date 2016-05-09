75046583489c2f72046addcd08336b83e7fd2e57
Erumpet <erumpet@gmail.com>
2016-05-08 21:29:04 -0500

    gather bash works

--- daemon/gather_bash.c
+++ daemon/gather_bash.c
@@ -9,45 +9,45 @@
 
 int main(){
 
-	//Constants
-	int MAX_BUF = 1024;
+	//	Constants
+	int MAX_BUF = 3072;
 	char buf[MAX_BUF];
 	
-	//Set up database connection
-	sqlite3 *db;
+	//	Set up database connection
+	/*sqlite3 *db;
 	char *zErrMsg = 0;
 	int rc;
 	rc = sqlite3_open("gather.db", &db);
 	if( rc ){
   	fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
   	exit(0);
-  }
+  }*/
   
-	//Set up pipes
+	//	Set up pipes
 	int fd_cpu;
 	int fd_disk;
 	int fd_mem;
-	fd_cpu = open("CPU_pipe", O_RDONLY );
-	fd_disk = open("disk_pipe", O_RDONLY );
-	fd_mem = open("memory_pipe", O_RDONLY );
+	fd_cpu = open("../IO/CPU_pipe", O_RDONLY );
+	fd_disk = open("../IO/disk_pipe", O_RDONLY );
+	fd_mem = open("../IO/memory_pipe", O_RDONLY );
 
-	//Loop and wait on pipes
+	//	Loop and wait on pipes
 	for(;;) 
 	{
-		if(read(fd_cpu, buf, MAX_BUF ) > 0 )
-			printf("Received: %s\n", buf);
-		if(read(fd_disk, buf, MAX_BUF ) > 0 )
-			printf("Received: %s\n", buf);
-		if(read(fd_mem, buf, MAX_BUF ) > 0 )
-			printf("Received: %s\n", buf);
+		while(read(fd_cpu, buf, MAX_BUF ) > 0 )
+			printf("Received cpu: %s\n", buf);
+		while(read(fd_disk, buf, MAX_BUF ) > 0 )
+			printf("Received disk: %s\n", buf);
+		while(read(fd_mem, buf, MAX_BUF ) > 0 )
+			printf("Received memory: %s\n", buf);
 
-		//Use data
+		//	Use data
 	}
 
-	//Clean up and exit
+	//	lean up and exit
 	close(fd_cpu);
 	close(fd_disk);
 	close(fd_mem);
-	sqlite3_close(db);
+	//sqlite3_close(db);
 	return 0;
 }
