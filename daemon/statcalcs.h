e1b21bfaed6b4a0ef49272e6030e4e56d4b8b094
lapin2 <lapin2@illinois.edu>
2016-05-10 19:31:00 -0500

    Add files via upload

---
+++ daemon/statcalcs.h
@@ -0,0 +1,24 @@
+#ifndef STATCALCS_HEADER_FILE
+#define STATCALCS_HEADER_FILE
+
+bool usegamma = false; //If we are using the gamma distribution then set this flag to true
+bool usenormal = false; //If we are using the normal distribution then set this flag to true
+bool useexponential = false; //If we are using the exponential distribution then set this flag to true
+
+void gammabootstrap(float *surrogatenum[], int *surrogatefreq[], float *gammaparam[2], int numboot); //Function for calculating the population gamma distribution parameters alpha and theta given the sample distribution
+
+void normalbootstrap(float *surrogatenum[], int *surrogatefreq[], float *normalparam[2], int numboot, int surrogatesize);
+
+void exponentialbootstrap(float *surrogatenum[], int *surrogatefreq[], float *exponentialtheta, int numboot, int surrogatesize)
+
+int factorial(int n) //Function for calculating factorial of integer n
+
+float gammacutoff(float alpha, float theta, float percent);
+
+float exponentialcutoff(float theta, float percent);
+
+float normalcutoff(float mu, float sigma, float percent);
+
+int main();
+
+#endif
