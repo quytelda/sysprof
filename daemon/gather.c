a2c7d4101571159aea877bf5f5e329b2feaec410
Erumpet <erumpet@gmail.com>
2016-05-12 22:15:05 -0500

    gather and statcalc

--- daemon/gather.c
+++ daemon/gather.c
@@ -106,8 +106,8 @@ int main(){
 		//	Insert newly collected data into database
 		char * sql_add = malloc(512);
 		snprintf(sql_add, 512, 
-			"INSERT INTO NET_DATA (ID,PAC_IN,UDP_IN,TCP_IN,ICMP_IN,OTHER_IN,PAC_OUT,UDP_OUT,TCP_OUT,ICMP_OUT,OTHER_OUT)"\
-			" VALUES (NULL, %u, %u, %u, %u, %u, %u, %u, %u, %u, %u);", current.pac_in, current.udp_in, current.tcp_in, 
+			"INSERT INTO NET_DATA (PAC_IN,UDP_IN,TCP_IN,ICMP_IN,OTHER_IN,PAC_OUT,UDP_OUT,TCP_OUT,ICMP_OUT,OTHER_OUT)"\
+			" VALUES (%u, %u, %u, %u, %u, %u, %u, %u, %u, %u);", current.pac_in, current.udp_in, current.tcp_in, 
 			current.icmp_in, current.other_in, current.pac_out, current.udp_out, current.tcp_out, current.icmp_out, current.other_out);
 		rc = sqlite3_exec(db, sql_add, NULL, 0, &zErrMsg);
 		if( rc != SQLITE_OK ){
